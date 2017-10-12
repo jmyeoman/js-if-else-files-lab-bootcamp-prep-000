d4d305c7dce46e8e8ef4a0d90008ebf02fa60ad6
jmyeoman <jared.yeoman@gmail.com>
2017-10-12 03:00:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var name = "Susan"
 var height = "74"
 if (name === "Susan") {
   console.log("The name is correct")
-} 
-  else if (name === "Joe") {
+} else {
     console.log("Expected the name to be Susan")
   }
 
